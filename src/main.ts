53db633b177061dd0166a53789976870c0dbfbb2
Luiz Guandalini <luizluizcorinthians@gmail.com>
2025-12-24 19:51:06 -0300

    adicao de configuracao no backend de quantidade de laudos em X estado

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
   // Habilita CORS
   app.enableCors({
